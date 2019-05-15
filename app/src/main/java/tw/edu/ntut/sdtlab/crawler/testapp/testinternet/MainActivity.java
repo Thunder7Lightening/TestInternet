edc4d8343ac7f1b873e7821d508b63336bf1fb6d
劉宸宗 <1101105310@gm.kuas.edu.tw>
2019-05-15 14:46:54 +0800

    Initial commit, behave like TestWifi

---
+++ app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testinternet/MainActivity.java
@@ -0,0 +1,56 @@
+package tw.edu.ntut.sdtlab.crawler.testapp.testinternet;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(true);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(wifiStateReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(wifiStateReceiver);
+    }
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
+
+            switch (wifiStateExtra) {
+                case WifiManager.WIFI_STATE_ENABLED:
+                    // todo: nothing currently
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    crash();
+                    break;
+            }
+        }
+    };
+
+    private void crash() {
+        int i = 1 / 0;
+    }
+}
