ee243f8cc1938892f8d6afb15a89a4e13cc8baff
劉宸宗 <1101105310@gm.kuas.edu.tw>
2019-05-15 16:49:28 +0800

    Simple get WebPage

--- app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testinternet/MainActivity.java
+++ app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testinternet/MainActivity.java
@@ -1,56 +1,42 @@
 package tw.edu.ntut.sdtlab.crawler.testapp.testinternet;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.WifiManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WifiManager wifiManager;
+    WebView t_webView1;
+    Button requestYahooPageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        wifiManager.setWifiEnabled(true);
-    }
+        t_webView1 = (WebView) findViewById(R.id.webView1);
+        requestYahooPageButton = (Button) findViewById(R.id.requestYahooPageButton);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
-        registerReceiver(wifiStateReceiver, intentFilter);
-    }
+        WebSettings webSettings = t_webView1.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSaveFormData(false);
+        webSettings.setSupportZoom(true);
+        t_webView1.setWebViewClient(new WebViewClient());
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        unregisterReceiver(wifiStateReceiver);
-    }
-
-    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
-
-            switch (wifiStateExtra) {
-                case WifiManager.WIFI_STATE_ENABLED:
-                    // todo: nothing currently
-                    break;
-                case WifiManager.WIFI_STATE_DISABLED:
-                    crash();
-                    break;
+        requestYahooPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t_webView1.loadUrl("https://tw.yahoo.com/");
             }
-        }
-    };
-
-    private void crash() {
-        int i = 1 / 0;
+        });
     }
+
 }
