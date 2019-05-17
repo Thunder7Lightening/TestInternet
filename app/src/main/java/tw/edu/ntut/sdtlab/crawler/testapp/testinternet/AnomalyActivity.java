3aa991be7e716203e133c3b4f844474e60149f87
劉宸宗 <1101105310@gm.kuas.edu.tw>
2019-05-17 21:53:07 +0800

    Add new page (Anomaly)

---
+++ app/src/main/java/tw/edu/ntut/sdtlab/crawler/testapp/testinternet/AnomalyActivity.java
@@ -0,0 +1,13 @@
+package tw.edu.ntut.sdtlab.crawler.testapp.testinternet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnomalyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anomaly);
+    }
+}
